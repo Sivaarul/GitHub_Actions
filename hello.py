37117ad0da482e2fb5e3217bc2aefedf35adb5cb
Sivaarul <sivaarul2017@gmail.com>
2025-06-22 11:24:00 +0530

    updated yml

--- hello.py
+++ hello.py
@@ -1,1 +1,8 @@
-print ('hello python')
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+# Add the numbers
+result = num1 + num2
+
+# Print the result
+print("The sum is:", result)
